0a4c71e317d36ad5e7e6511a3aad6a01a3798397
Megapreneur <ademolamegbabi@gmail.com>
2022-11-15 23:06:14 +0100

    subsequent commit

--- src/main/java/com/example/loan24/service/EmailNotificationServiceImpl.java
+++ src/main/java/com/example/loan24/service/EmailNotificationServiceImpl.java
@@ -1,7 +1,12 @@
 package com.example.loan24.service;
 
+import com.example.loan24.dto.request.EmailNotificationRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailNotificationServiceImpl implements EmailNotificationService{
+    @Override
+    public String sendHtmlMail(EmailNotificationRequest notificationRequest) {
+        return null;
+    }
 }
